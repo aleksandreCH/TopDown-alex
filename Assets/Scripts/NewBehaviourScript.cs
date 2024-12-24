1e895eedfb4420d82abae543233aa4a2825adfba
aleksandreCH <162612528+aleksandreCH@users.noreply.github.com>
2024-12-24 19:19:44 +0400

    alex

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sum(10, 7, "ჯამი უდრის ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private int Sum(int a, int b)
+    {
+        int result = a + b;
+        Debug.Log(jami udris, 17);
+
+
+    }
+    private void ShowMessage(string name, string lastname,)
+    {
+        string result = name + "" + lastname;
+        Debug.Log(name + lastname = alexandre chogovadze);
+    }
+    private void ShowMessage(string name, string lastname, int name)
+string result = name + "" + lastname + "" + name;
+    Debug.Log(name + lastname + age = alexandre chogovadze 13);
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+
+[SerializeField] float speed = 10f;
+[SerializeField] Camera camera;
+
+ void Update() {
+
+    float horizontal = Input.GetAxisRaw("Horizontal"); 
+    float vertical = Input.GetAxisRaw("Vertical");
+
+
+    transform.position  = transform.position + new Vector3(horizontal, vertical) * Time.deltaTime * speed;
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+    
+
+
+
+    
+    
+
+
+
+    
+
+
