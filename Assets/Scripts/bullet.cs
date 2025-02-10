9ee3328a9a73f734a328527071cdd8445fbac905
aleksandreCH <162612528+aleksandreCH@users.noreply.github.com>
2025-02-10 19:05:27 +0400

    alex

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -6,9 +6,11 @@ public class bullet : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
 
+    [SerializeField] float bulletspeed = 20f;
+
     void Start()
     {
-        rb.AddForce(transform.up * 20f, ForceMode2D.Impulse);
-        // Destroy(gameObject, 3f);
+        rb.AddForce(transform.up * bulletspeed, ForceMode2D.Impulse);
+        Destroy(gameObject, 1f);
     }
 }
